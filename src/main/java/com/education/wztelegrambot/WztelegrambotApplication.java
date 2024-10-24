c07354b25627eb8f8dd734a39b6145fd808815c2
ПК <saponembigas@gmail.com>
2024-10-24 20:48:32 +0700

    init

---
+++ src/main/java/com/education/wztelegrambot/WztelegrambotApplication.java
@@ -0,0 +1,13 @@
+package com.education.wztelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WztelegrambotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WztelegrambotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/education/wztelegrambot/WztelegrambotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.education.wztelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WztelegrambotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
